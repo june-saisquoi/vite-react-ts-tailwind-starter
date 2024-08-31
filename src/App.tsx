c997b987f03b521f0a2737e86747c75e20ce9592
junhyejeon <junhye.jeon@gmail.com>
2024-09-01 03:18:41 +0900

    restarted

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import A from "./components/A"
+
+const App = () => {
+  return (
+    <>
+        <h1>App Components</h1>
+        <A />
+    </>
+  )
+}
+export default App
---
+++ src/components/A.tsx
@@ -0,0 +1,8 @@
+const A = () => {
+  return (
+    <>
+      <h1>A Components</h1>
+    </>
+  )
+}
+export default A
